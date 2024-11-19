daea2d42a4818c6acd99741566798fe87f183019
xufuying <xufuying@100doc.com.cn>
2024-11-20 00:07:46 +0800

    修复naive-ui grid布局服务端渲染页面不动态适配问题

--- pages/index.vue
+++ pages/index.vue
@@ -2,21 +2,27 @@
 import { useStorage } from "@vueuse/core";
 import { type IRoute } from "~/types";
 const hots = useStorage<Array<IRoute>>("hots", []);
+const show = ref<boolean>(true);
+onMounted(() => {
+  show.value = false;
+  setTimeout(() => {
+    show.value = true;
+  }, 0);
+});
 </script>
 
 <template>
-  <ClientOnly>
-    <n-grid
-      x-gap="16"
-      y-gap="16"
-      responsive="screen"
-      cols="2xl:6 xl:5 l:4 m:3 s:2 1"
-    >
-      <n-gi :key="route.path" v-for="route in hots">
-        <HotCard :route="route" />
-      </n-gi>
-    </n-grid>
-  </ClientOnly>
+  <n-grid
+    v-if="show"
+    x-gap="16"
+    y-gap="16"
+    responsive="screen"
+    cols="2xl:6 xl:5 l:4 m:3 s:2 1"
+  >
+    <n-gi :key="route.path" v-for="route in hots">
+      <HotCard :route="route" />
+    </n-gi>
+  </n-grid>
 </template>
 
 <style lang="scss" scoped></style>
